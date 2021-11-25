a247edda59293c4d3a636a99041803bea215fa3d
Jolow <jolow999@gmail.com>
2021-11-25 11:34:39 +0800

    Added create_tickets.py which populates test account with 100 tickets defined in tickets.json

---
+++ create_tickets/create_tickets.py
@@ -0,0 +1,20 @@
+import json 
+import requests
+
+with open('create_tickets/tickets.json') as tickets, open('create_tickets/config.json') as config:
+    tickets = json.load(tickets)
+    config = json.load(config)
+
+subdomain = config['subdomain']
+auth = config['authentication']
+
+url = f"https://{subdomain}.zendesk.com/api/v2/imports/tickets/create_many.json"
+payload=json.dumps(tickets)
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': f'Basic {auth}',
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
